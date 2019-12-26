0d3eccb178c9112b4f6c1c537ae719705392223e
Leonardo Cardozo Ferreira <leonardo329cf@gmail.com>
2019-12-26 18:03:24 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/example/demo/entities/pk/OrderItemPK.java
+++ src/main/java/com/example/demo/entities/pk/OrderItemPK.java
@@ -34,6 +34,8 @@ public class OrderItemPK implements Serializable {
 	public void setProduct(Product product) {
 		this.product = product;
 	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
